feddd5fe99e5bf7435f9a3c7682e67144f4ff7f9
HyojunSon <44393225+HyojunSon@users.noreply.github.com>
2025-02-10 00:05:13 +0900

    Update pitch.py

--- pitch.py
+++ pitch.py
@@ -41,6 +41,11 @@ webrtc_ctx = webrtc_streamer(
     media_stream_constraints={"video": False, "audio": True},
 )
 
+# 실시간 분석 및 그래프 업데이트
+if webrtc_ctx.state.playing:
+    pitches = []
+    while True:
+        
 # 그래프 초기화
 fig, ax = plt.subplots()
 line, = ax.plot([], [])
